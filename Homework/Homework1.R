040e05705fdfa94d69f9efbccefc0859e473eaaa
Jeremy Maslanko <maslankoj@gmail.com>
2024-02-11 14:04:20 -0500

    update gitignore and R code

--- Homework/Homework1.R
+++ Homework/Homework1.R
@@ -1,5 +1,5 @@
 library(dplyr)
-
+library(ggplot2)
 data <- read.csv(file="/Users/Jeremy/Documents/GitHub/multivariate/data/TrackRecords.csv", header = TRUE, sep = ",")
 
 # May need to multiply/divide cols to get consistent scale
@@ -9,10 +9,11 @@ meanVector <- unname(colMeans(data[-1]))
 sdVector <- unname(apply(data[-1], 2, sd))
 R <- cor(data[-1])
 
+summary(data[-1])
 # Spread of data increases the most for the marathon
-boxplot(data[-1])
+boxplot(data[-1], main="Figure 1: Box and Whisker")
 
-pairs(data[-1])
+pairs(data[-1], main="Figure 2: Correlation Matrix")
 
 # View distributions
 # Less normally distributed as the distance increases
@@ -24,4 +25,7 @@ hist(data$X1500m..m.)
 hist(data$X3000m..m.)
 hist(data$Marathon..m.)
 
+par(mfrow = c(1, 2))
+plot(density(data$X100m..s.), main="Density plot for 100m")
+plot(density(data$Marathon..m.), main="Density plot for Marathon")
 
