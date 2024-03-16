dbdbdf15b9511b34438d35f15aebb3c368e18f04
Jeremy Maslanko <maslankoj@gmail.com>
2024-03-16 13:01:58 -0400

    add multivariate graphs

--- Homework/Homework3.R
+++ Homework/Homework3.R
@@ -7,6 +7,14 @@ S <- matrix(c(5, 4, 4, 5), nrow = 2)
 xbar<-apply(S,2,mean)
 Sigma<-var(S)
 
+eigen_info <- eigen(S)
+eigenvalues <- eigen_info$values
+eigenvectors <- eigen_info$vectors
+
+ellipse(center = xbar, shape = S, radius = 1,
+        angle = atan2(eigenvectors[2, 1], eigenvectors[1, 1]),
+        col = "blue")
+
 par(bg='white')
 
 
@@ -35,3 +43,95 @@ par(bg='white')
 plot(0, 0, type = "n", xlim = c(-8, 8), ylim = c(-8, 8), xlab = "X", ylab = "Y")
 ellipse(center = xbar, shape = S, radius = 1, col = "blue", border = "blue", lwd = 2)
 
+################
+## Question 2 ##
+################
+
+# Generate 1000 random samples from normal distribution
+x1 <- rnorm(1000, mean = 0, sd = 1)
+
+# Plot histogram of random samples
+hist(x1, breaks = 30, main = "Histogram of Random Samples",
+     xlab = "Random Samples", ylab = "Frequency", col = "skyblue", border = "white")
+
+x2 <- ifelse(abs(x1) < 1, -x1, x1)
+hist(x2, breaks = 30, main = "Histogram of Random Samples",
+     xlab = "Random Samples", ylab = "Frequency", col = "skyblue", border = "white")
+
+
+# Plot joint distribution
+plot(x1, x2, 
+     xlab = "x1", ylab = "x2", 
+     main = "Joint Distribution of Two Samples", col = "blue", pch = 19)
+
+################
+## Question 5 ##
+################
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(gplots)
+
+data <- read.csv("/Users/Jeremy/Documents/GitHub/multivariate/data/air_pollution.csv")
+x <- data[,2:8]
+#x <- as.matrix(x)
+matrix <- as.matrix(x)
+
+## 2-d scatter plots
+pairs(x)
+
+# box plots
+par(mfrow = c(2, 4))
+boxplot(matrix[,1],main="Wind")
+boxplot(matrix[,2],main="Solar")
+boxplot(matrix[,3],main="CO")
+boxplot(matrix[,4],main="NO")
+boxplot(matrix[,5],main="NO2")
+boxplot(matrix[,6],main="O3")
+boxplot(matrix[,7],main="HC")
+
+# histogram plots
+par(mfrow = c(2, 4))
+hist(matrix[,1],main="Wind",xlab="",col="blue", border="pink")
+hist(matrix[,2],main="Solar",xlab="",col="blue", border="pink")
+hist(matrix[,3],main="CO",xlab="",col="blue", border="pink")
+hist(matrix[,4],main="NO",xlab="",col="blue", border="pink")
+hist(matrix[,5],main="NO2",xlab="",col="blue", border="pink")
+hist(matrix[,6],main="O3",xlab="",col="blue", border="pink")
+hist(matrix[,7],main="HC",xlab="",col="blue", border="pink")
+
+# Q-Q plots univariate for each feature
+par(mfrow = c(2, 4))
+qqnorm(matrix[,1],main="Wind"); qqline(x[,1],col=2)
+qqnorm(matrix[,2],main="Solar"); qqline(x[,2],col=2)
+qqnorm(matrix[,3],main="CO"); qqline(x[,3],col=2)
+qqnorm(matrix[,4],main="NO"); qqline(x[,4],col=2)
+qqnorm(matrix[,5],main="NO2"); qqline(x[,5],col=2)
+qqnorm(matrix[,6],main="O3"); qqline(x[,6],col=2)
+qqnorm(matrix[,7],main="HC"); qqline(x[,7],col=2)
+
+
+# QQ Plot multivariate (this is checking for normality assumption)
+p <- ncol(x); n<-nrow(x)
+S <- var(x)
+xbar <- apply(x, 2, mean)
+
+D2 <- NULL
+for (i in 1:n){
+  d2 <- as.matrix(x[i,]-xbar) %*% solve(S) %*% t(x[i,]-xbar)
+  D2 <- c(D2, d2)
+}
+D2 <- sort(D2)
+qqplot(qchisq((1:n-.5)/n, p), D2, pch=21, bg="blue")
+abline(0,1,col=2) ### intercept 0, slope 1
+title("A Chi-square Q-Q Plot")
+
+# Shapiro Wilks test, reject null that our data is normally distributed
+mvnormtest::mshapiro.test(t(x))
+
+
+
+
+# ACF Plot
+acf(D2)
